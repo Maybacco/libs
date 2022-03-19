6f70ac81d86307c3f5a934e01d8248c4f08807ca
Alberto Mulazzani <a_mulazzani@yahoo.it>
2022-03-19 08:40:39 +0100

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
+
+-- client scripts
+client_scripts{
+    "lib/utils.lua",
+    "lib/weapons_data.lua"
+  }
+  
+  -- client files
+  files{
+    "lib/Luaoop.lua",
+    "lib/IDManager.lua",
+    "lib/Tools.lua",
+    "lib/Debug.lua",
+    "lib/TunnelV2.lua",
+    "lib/ProxyV2.lua",
+  }
+
